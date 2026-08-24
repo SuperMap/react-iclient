f00876f5e261bf5e5e88c01b27acb3dc0cc45b93
songyumeng <songyumeng@supermap.com>
2026-08-24 10:05:24 +0800

    【fix】修改高版本nodenpm获取npm_config_argv的方法 AI-GEN: 80% Claude

--- config/publish.js
+++ config/publish.js
@@ -1,9 +1,26 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-let argv = JSON.parse(process.env['npm_config_argv']);
-let origin = argv.original;
-let key = origin[1] ? origin[1].replace('-', '') : 'mapboxgl';
+function getPublishKey() {
+  if (process.env['npm_config_mapboxgl'] === 'true') {
+    console.log('npm_config_mapboxgl is true, using mapboxgl');
+    return 'mapboxgl';
+  }
+
+  let argv = {};
+  try {
+    argv = JSON.parse(process.env['npm_config_argv'] || '{}');
+  } catch (error) {
+    console.log('Failed to parse npm_config_argv:', process.env['npm_config_argv'], process.argv.slice(2));
+    argv = {};
+  }
+
+  let option = (argv.original || process.argv.slice(2)).find(arg => arg.startsWith('-'));
+  const key = option ? option.replace(/^-+/, '') : 'mapboxgl';
+  return key;
+}
+
+let key = getPublishKey();
 
 fs.copy(path.resolve(__dirname, `../src/${key}/package-config.json`), path.resolve(__dirname, `../package.json`), err => {
   if (err) throw err;
